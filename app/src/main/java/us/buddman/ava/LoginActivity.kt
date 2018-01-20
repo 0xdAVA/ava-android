e54d7f203a6213b1fd08d13a2d2cc767b819e67e
Junseok Oh <kotohana5706@edcan.kr>
2018-01-21 01:53:41 +0900

    Implement Activity

---
+++ app/src/main/java/us/buddman/ava/LoginActivity.kt
@@ -0,0 +1,16 @@
+package us.buddman.ava
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import us.buddman.ava.utils.BaseActivity
+import us.buddman.ava.utils.NetworkHelper
+
+class LoginActivity : BaseActivity() {
+
+    override fun setDefault() {
+
+    }
+
+    override val viewId: Int = R.layout.activity_login
+    override val toolbarId: Int = 0
+}
